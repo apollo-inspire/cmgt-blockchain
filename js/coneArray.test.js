e06777a7c6037252b2f5a110666131eecca003c8
LucasvdVegt <lpsvandervegt@gmail.com>
2024-02-16 16:07:05 +0100

    Add testing with jest

---
+++ js/coneArray.test.js
@@ -0,0 +1,13 @@
+const cloneArray = require('./cloneArray')
+
+test('properly clone array', () => {
+    const array = [1, 2, 3];
+
+    expect(
+        cloneArray(array)
+    ).toEqual(array)
+
+    expect(
+        cloneArray(array)
+    ).not.toBe(array)
+})
