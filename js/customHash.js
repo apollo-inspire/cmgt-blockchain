8231e5b0d83d8bd757d7ecda8d96f77aa93a4d6c
LucasvdVegt <lpsvandervegt@gmail.com>
2024-02-22 20:16:10 +0100

    split into diffrent unit tests

--- js/customHash.js
+++ js/customHash.js
@@ -5,23 +5,23 @@ function customHash(input){
 
 
     // Stap 2: Alle getallen worden gesplitst en los in een array
-    let numberArray = "";
+    let numberArray = [1,1,6,1,0,1,1,2,0,1,1,6];
 
     // Stap 3: Array wordt opgesplitst in blokken van 10 getallen
-    let blocks = "";
+    let blocks = [[1,1,6,1,0,1,1,2,0,1],[1,6]];
 
 
-    // Stap 4: Aanvullen van blokken als nodig
-    let blocks2 = "";
+    // Stap 5.1: Aanvullen van blokken met getallen als nodig
+    let blocks2 = [[1,1,6,1,0,1,1,2,0,1],[1,6,0,1,2,3,4,5,6,7]];
 
 
-    // Stap 5: Het eerste blok wordt bij het tweede blok opgeteld
-    let hasArray = [2,7,6,2,2,4,5,7,6,8];
+    // Stap 5: Het eerste blok wordt bij het tweede blok opgeteld (herhaal)
+    let hashArray = [2,7,6,2,2,4,5,7,6,8];
 
     // Stap 6: Vormen van de hash string
     let hash = "d0b3cb0cc9100ef243a1023b2a129d15c28489e387d3f8b687a7299afb4b5079";
 
-    return { hash, hasArray };
+    return { hash, asciiValues, numberArray, blocks, blocks2, hashArray };
 }
 
 let input = "text";
--- js/customHash.test.js
+++ js/customHash.test.js
@@ -1,17 +1,69 @@
 const customHash = require('./customHash');
 
-test('full working customHash function', () => {
+test('full working customHash function, from step 1 to step 6', () => {
     const input = 'text';
 
     expect(
         customHash(input).hash
     ).toEqual('d0b3cb0cc9100ef243a1023b2a129d15c28489e387d3f8b687a7299afb4b5079');
 
-    // expect(
-    //     functionInstance.asciiValues
-    // ).toEqual([116,101,120,116])
+})
+
+
+
+
+test('working part 1 of customHash function, text to ascii value', () => {
+    const input = 'text';
 
     expect(
-        customHash().hasArray
+        customHash().asciiValues
+    ).toEqual([116,101,120,116]);
+
+})
+
+
+test('working part 2 of customHash function, split into single numbers', () => {
+    const input = 'text';
+
+    expect(
+        customHash().numberArray
+    ).toEqual([1,1,6,1,0,1,1,2,0,1,1,6]);
+
+})
+
+test('working part 3 of customHash function, split into blocks of 10 numbers', () => {
+    const input = 'text';
+
+    expect(
+        customHash().blocks
+    ).toEqual([[1,1,6,1,0,1,1,2,0,1],[1,6]]);
+
+})
+
+
+test('working part 5.1 of customHash function, add numbers to blocks until total of 10 when needed', () => {
+    const input = 'text';
+
+    expect(
+        customHash().blocks2
+    ).toEqual([[1,1,6,1,0,1,1,2,0,1],[1,6,0,1,2,3,4,5,6,7]]);
+
+})
+
+test('working part 5 of customHash function, add first block to second block in array', () => {
+    const input = 'text';
+
+    expect(
+        customHash().hashArray
     ).toEqual([2,7,6,2,2,4,5,7,6,8]);
+
+})
+
+test('working part 6 of customHash function, hash 10 numbers to with SHA256', () => {
+    const input = 'text';
+
+    expect(
+        customHash().hash
+    ).toEqual("d0b3cb0cc9100ef243a1023b2a129d15c28489e387d3f8b687a7299afb4b5079");
+
 })
